73ef1e52264893bda2b841e9bd6f23ffcd1e911b
TAEWON KO <migmaknoob@gmail.com>
2024-12-27 18:26:03 +0900

    1

---
+++ src/num2775.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class num2775 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder(); // 스트링빌더를 써야지 효율이 좋음.
+
+        int[][] array = new int[15][15];
+
+        for (int i = 1; i <= 14; i++) {
+            array[0][i] = i;
+        }
+        for (int j = 1; j <= 14; j++) {
+            for (int k = 1; k <= 14; k++) {
+                array[j][k] = array[j][k - 1] + array[j - 1][k];
+            }
+        }
+
+        for (int i = 0; i < T; i++) {
+            int K = Integer.parseInt(br.readLine());
+            int N = Integer.parseInt(br.readLine());
+            sb.append(array[K][N] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
